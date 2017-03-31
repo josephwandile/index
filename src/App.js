8d06a2d82ab9dbdb24df940a609c55ccf7f3077c
Joseph Kahn <josephkahn@college.harvard.edu>
2017-03-31 03:16:44 -0400

    getting started

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import cover from './images/cover.jpg';
 import '../node_modules/bulma/css/bulma.css';
 import '../node_modules/font-awesome/css/font-awesome.css';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App section">
-        <div className="container">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div>
+        <div className="intro hero is-fullheight">
+          <div className="hero-head">
+            <img className="cover-image" src={cover} alt="Totally a candid photo taken in the Bo-Kaap"/>
+          </div>
+          <div className="hero-body">
+            <div className="container has-text-centered">
+              <h1 className="title">Joe Kahn</h1>
+              <h2 className="subtitle">Harvard CS and MP at Dorm Room Fund</h2>
+              <div className="icons">
+                <a className="icon is-medium" href="https://www.linkedin.com/in/josephwandile"><i className="fa fa-linkedin"></i></a>
+                <a className="icon is-medium" href="https://www.github.com/josephwandile"><i className="fa fa-github"></i></a>
+                <a className="icon is-medium" href="https://www.twitter.com/josephwandile"><i className="fa fa-twitter"></i></a>
+                <a className="icon is-medium" href="https://medium.com/@josephwandile"><i className="fa fa-medium"></i></a>
+              </div>
+            </div>
+          </div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <i className='fa fa-github'/>
-        </p>
       </div>
     );
   }
